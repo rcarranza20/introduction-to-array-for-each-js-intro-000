31f1de823ab3623b5761c5205b4c9c4e87ebb0bb
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-22 19:45:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray(array,callback){
+  Array.prototype.forEach(callback);
+}
 
 // Add your changeCompletely() function here:
